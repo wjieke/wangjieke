43b9d8f618647acb4383710db61dcefdad3107f0
wangjieke <784622884@qq.com>
2020-12-04 15:04:30 +0800

    增加博客模块

---
+++ src/Solution/Model/Entity/Blog/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Entity.Blog
+{
+    /// <summary>
+    /// 文章
+    /// </summary>
+    public class Article
+    {
+
+    }
+}
